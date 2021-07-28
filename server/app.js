18109c9160d1f58d722fd74acf8d24403ddfc069
Jebbit-koi <jjooh10@naver.com>
2021-07-28 22:38:38 +0900

    updated app.js

--- server/app.js
+++ server/app.js
@@ -3,7 +3,7 @@ import cors from 'cors';
 import morgan from 'morgan';
 import helmet from 'helmet';
 import 'express-async-errors';
-import tweetRouter from './router/tweet';
+import tweetsRouter from './router/tweets.js';
 
 const app = express();
 
@@ -12,6 +12,16 @@ app.use(helmet());
 app.use(cors());
 app.use(morgan('tiny'));
 
-app.use('/tweets', tweetRouter);
+app.use('/tweets', tweetsRouter);
+
+app.use((req, res, next) => {
+    res.sendStatus(404);
+})
+
+app.use((error, req, res, next) => {
+    console.error(error);
+    res.sendStatus(500);
+})
+
 
 app.listen(8080);
